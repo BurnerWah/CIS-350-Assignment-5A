1052967f8dec74b250c1a83c467ff3d060f30eb9
Jaden <jpleasants@mail.bradley.edu>
2022-03-06 14:43:04 -0600

    Fixed some stuff with project settings mostly

--- Assets/CollectableGem/Scripts/GemBehaviour.cs
+++ Assets/CollectableGem/Scripts/GemBehaviour.cs
@@ -1,40 +1,40 @@
-﻿using System.Collections;
+﻿/*
+ * Jaden Pleasants
+ * Assignment 5A
+ * Defines behavoir for the gem to follow
+ * (This was modified since it was already in the codebase)
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GemBehaviour : MonoBehaviour
-{
-	[Header("References")]
-	public GameObject gemVisuals;
-	public GameObject collectedParticleSystem;
-	public CircleCollider2D gemCollider2D;
+public class GemBehaviour : MonoBehaviour {
 
-	private float durationOfCollectedParticleSystem;
+    [Header("References")]
+    public GameObject gemVisuals;
+    public GameObject collectedParticleSystem;
+    public CircleCollider2D gemCollider2D;
 
+    private float durationOfCollectedParticleSystem;
 
-	void Start()
-	{
-		durationOfCollectedParticleSystem = collectedParticleSystem.GetComponent<ParticleSystem>().main.duration;
-	}
+    void Start() {
+        durationOfCollectedParticleSystem = collectedParticleSystem.GetComponent<ParticleSystem>().main.duration;
+    }
 
-	void OnTriggerEnter2D(Collider2D theCollider)
-	{
-		if (theCollider.CompareTag ("Player")) {
-			GemCollected ();
-		}
-	}
+    void OnTriggerEnter2D(Collider2D theCollider) {
+        if (theCollider.CompareTag("Player")) {
+            GemCollected();
+        }
+    }
 
-	void GemCollected()
-	{
-		gemCollider2D.enabled = false;
-		gemVisuals.SetActive (false);
-		collectedParticleSystem.SetActive (true);
-		Invoke ("DeactivateGemGameObject", durationOfCollectedParticleSystem);
+    void GemCollected() {
+        gemCollider2D.enabled = false;
+        gemVisuals.SetActive(false);
+        collectedParticleSystem.SetActive(true);
+        Invoke("DeactivateGemGameObject", durationOfCollectedParticleSystem);
+    }
 
-	}
-
-	void DeactivateGemGameObject()
-	{
-		gameObject.SetActive (false);
-	}
+    void DeactivateGemGameObject() {
+        gameObject.SetActive(false);
+    }
 }
---
+++ Assets/Misc/Scripts/ScoreUtil.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Jaden Pleasants
+ * Assignment 5A
+ * Manages score
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUtil : MonoBehaviour {
+    private int score = 0;
+
+    public Text scoreText;
+
+    private void Start() {
+        score = 0;
+        scoreText.text = $"Score: {score}";
+    }
+
+    public void AddPoint() {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+}
